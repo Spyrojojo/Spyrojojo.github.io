3d9d1ff549d3e5fbacdde7f819b157cad60be2d2
spyrojojo <Spyrojojo@outlook.fr>
2017-08-09 23:24:22 +0200

    Ramat

---
+++ Ramat/offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1502313792,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"background-sheet0.png",
+		"logo-sheet0.png",
+		"play-sheet0.png",
+		"reset-sheet0.png",
+		"sprite-sheet0.png",
+		"particles.png",
+		"obstacle_bas-sheet0.png",
+		"obstacle_haut-sheet0.png",
+		"player-sheet0.png",
+		"player-sheet1.png",
+		"bulle_1.ogg",
+		"bulle_2.ogg",
+		"bulle_3.ogg",
+		"ah_1.ogg",
+		"ah_2.ogg",
+		"otest.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
